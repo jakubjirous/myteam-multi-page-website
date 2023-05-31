0b2f40dee64b9e506979e72197f0fc97c1b910ce
Jakub Jirous <info@jakubjirous.cz>
2023-05-31 10:57:44 -0700

    [astro] install tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		screens: {
+			// sm: "640px",
+			// md: "768px",
+			// lg: "1024px",
+			// xl: "1280px",
+			// "2xl": "1536px",
+
+			tablet: "768px",
+			desktop: "1440px"
+		},
+		extend: {
+			colors: {
+				// primary
+				sherpa: "#014E56",
+				flory: "#F67E7E",
+
+				// secondary
+				baked: "#79C8C7",
+				casul: "#2C6269",
+				cyprus: "#004047",
+				daintree: "#012F34",
+				swamp: "#002529"
+			},
+			fontSize: {
+				13: ["0.813rem", { lineHeight: "1.125rem" }],
+				15: ["0.938rem", { lineHeight: "1.563rem" }],
+				18: ["1.125rem", { lineHeight: "1.75rem" }],
+				32: ["2rem", { lineHeight: "2rem" }],
+				40: ["2.5rem", { lineHeight: "2.5rem" }],
+				48: ["3rem", { lineHeight: "3rem" }],
+				64: ["4rem", { lineHeight: "3.5rem" }],
+				100: ["6.25rem", { lineHeight: "6.25rem" }]
+			}
+		}
+	},
+	plugins: []
+};
