7298392148f900f2f6ef42d7dc069675558fabec
Jakub Jirous <info@jakubjirous.cz>
2023-05-31 11:06:31 -0700

    [astro] install mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind(), image()]
+	integrations: [react(), tailwind(), image(), mdx()]
 });
