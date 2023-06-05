a463d6db1cada9ed27f5d30829ca8a32f53d21e7
Jakub Jirous <info@jakubjirous.cz>
2023-06-05 10:01:46 -0700

    [astro] try to use sharp for images

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind(), image(), mdx()],
+	integrations: [react(), tailwind(), image({ serviceEntryPoint: "@astrojs/image/sharp" }), mdx()],
 	output: "server",
 	root: "./",
 	adapter: vercel(),
